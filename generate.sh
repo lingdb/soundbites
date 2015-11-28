50de46384b5ecfd30c9ae6d8f9870169a99b4e42
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-28 09:44:23 +0100

    Add {rsync,wget} to generate.sh

--- generate.sh
+++ generate.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 # This script will fill the output folder as good as it can.
+# Give the target to fetch information from as $1.
+# Finding output directory:
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+output="$dir/output"
+# Copying static from submodule:
+rsync -r --progress ../soundcomparisons/soundcomparisons/static $output
+# Fetching content from url:
+wget "$1" -O $output/index.html
